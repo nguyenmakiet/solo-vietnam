4494fb5ef3956ce06cb63e35f35d64f4dfb7109d
Anh Kiet <anhkietnguyenminh@gmail.com>
2026-09-22 13:40:09 +0700

    update locations

--- data/locations/phu-quoc-night-market.ts
+++ data/locations/phu-quoc-night-market.ts
@@ -10,7 +10,7 @@ export const phuQuocNightMarket: Location = {
   lat: 10.2158553,
   lng: 103.9602497,
   address: "6 Bạch Đằng, Dương Đông, Phú Quốc, Kiên Giang (at the Bạch Đằng - Nguyễn Đình Chiểu junction)",
-  type: ["market", "nightlife"],
+  type: ["attraction", "city"],
   categories: [],
   experiences: ["food", "markets", "nightlife", "walking-tour"],
   tags: ["🌙 Night Market", "🦞 Seafood", "🛍️ Shopping", "🍢 Street Food"],
